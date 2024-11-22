1a3503f37e1d15c9d155ac450b35d0e13b6534c2
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-11-22 06:13:08 -0500

    Added keyboard handler

--- src/components/editableBlock.tsx
+++ src/components/editableBlock.tsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect, useRef } from 'react'
 import ContentEditable from 'react-contenteditable'
 
 
-function EditableBlock(id, html: initialHtml, tag: initialTag, updatePage) {
-    const [html, setHtml] = useState(initialHtml || '');
-    const [tag, setTag] = useState(initialTag || 'p');
-
-    const contentEditable = useRef<HTMLElement>(null);
+function EditableBlock(id, initialHtml: initialHtml, initialTag: initialTag, updatePage) {
+    const [html, setHtml] = useState(initialHtml || '')
+    const [htmlBackup, setHtmlBackup] = useState(null)
+    const [tag, setTag] = useState(initialTag || 'p')
+    const [previousKey, setPreviousKey] = useState('')
+    const contentEditable = useRef<HTMLElement>(null)
 
 
     useEffect(function () {
@@ -19,9 +20,37 @@ function EditableBlock(id, html: initialHtml, tag: initialTag, updatePage) {
     }, [html, tag, id, updatePage])
 
 
+    function onKeyDownHandler(e: React.KeyboardEvent<HTMLDivElement>) {
+        if (e.key == '/') {
+            setHtmlBackup(html)
+        }
+
+        if (e.key == 'Enter') {
+            if (previousKey != 'Shift') {
+                e.preventDefault()
+                addBlock({
+                    id,
+                    ref: contentEditable.current
+                })
+            }
+        }
+
+        if (e.key == 'Backspace' && !html) {
+            e.preventDefault()
+            deleteBlock({
+                id,
+                ref: contentEditable.current
+            }) 
+        }
+
+        setPreviousKey(e.key)
+    }
+
+
     function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
         setHtml(e.target.value)
     }
+
 
     return (
         <ContentEditable
